1cfd65c826484278f7045bec02d9bb2e5253301a
ethanjoh <ethan.joh@gmail.com>
2026-06-25 09:14:26 +0900

    회사소개 이미지, 문단 간격 조정

--- about-us.php
+++ about-us.php
@@ -1,101 +1,110 @@
-<?php include "include/header.php";?>
+<?php include "include/header.php"; ?>
 
-			<div role="main" class="main">
-				<section class="page-header page-header-classic page-header-sm">
-					<div class="container">
-						<div class="row">
-							<div class="col-md-8 order-2 order-md-1 align-self-center p-static">
-								<h1 data-title-border>인사말</h1>
-							</div>
-							<div class="col-md-4 order-1 order-md-2 align-self-center">
-								<ul class="breadcrumb d-block text-md-right">
-									<li><a href="index.php">Home</a></li>
-									<li class="active">회사소개</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-				</section>
-
-				<div class="container pb-1">
-
-					<div class="row pt-4">
-						<div class="col-lg-12 mb-4">
-							<h2 class="word-rotator slide font-weight-bold text-8 mb-0 appear-animation" data-appear-animation="maskUp">
-									<span>문서파쇄 전문</span>
-							</h2>											
-							<div class="overflow-hidden">
- 								<p>향우실업(주)는 대한민국 최고&#183;최대 안보단체 <span class="alternative-font"><a href="http://www.korva.or.kr" target="_blank">대한민국재향군인회</a></span>가 100% 출자하여 1978년에 설립한 회사입니다.<br>
-						        1970년대 정부로부터 폐지처리 대행기관으로 지정받아 오랜 시간 공공기관 폐문서처리를 담당해왔고, 40여년이라는 짧지않은 시간을 거쳐 문서파쇄 전문기업으로 국내 선두의 자리에 섰습니다.<br>
-							    </p>
-							</div>
-						</div>
-					</div>
-
-                    <!-- 2nd paragraph -->
-					<div class="row pt-4">
-						<div class="col">
-							<div class="overflow-hidden mb-3">
-								<h2 class="word-rotator slide font-weight-bold text-8 mb-0 appear-animation" data-appear-animation="maskUp">
-									<span>자원재활용을 넘어 정보보안서비스로</span>
-								</h2>
-							</div>
-						</div>
-					</div>
-					<div class="row mb-2">
-						<div class="col-lg-10">
-							<div class="overflow-hidden">
-								<p>2000년대 개인정보의 중요성이 대두되면서 단순 폐지처리가 아닌 신뢰를 바탕으로 한 보안문서 처리를 전문으로 하는 튼실한 기업이 요구되고 있습니다.<br>
-							    향우실업(주)는 고객들의 그러한 요구와 믿음을 기반으로 성장해왔고, 단순 재활용을 넘어 개인정보 폐기를 위한 정보보안서비스 전문기업으로 발전하였습니다.</p>
-							</div>
-						</div>
-						<div class="col-lg-2 appear-animation" data-appear-animation="fadeIn" data-appear-animation-delay="450">
-							<span class="thumb-info thumb-info-side-image thumb-info-side-image-right thumb-info-no-zoom">
-								<span class="thumb-info-side-image-wrapper">
-									<img src="img/about/bundle.jpg" class="img-fluid" alt="폐지묶음" style="width: 200px;">
-								</span>
-							</span>
-						</div>
-					</div>
-					<!-- 2nd paragraph end -->
-
-                    <!-- 3rd paragraph -->
-					<div class="row pt-4">
-						<div class="col">
-							<div class="overflow-hidden mb-3">
-								<h2 class="word-rotator slide font-weight-bold text-8 mb-0 appear-animation" data-appear-animation="maskUp">
-									<span>국가유공자와 함께 하는 기업</span>
-								</h2>
-							</div>
-						</div>
-					</div>
-					<div class="row mb-2">
-						<div class="col-lg-10">
-							<div class="overflow-hidden">
-								<p class="lead mb-2 appear-animation" data-appear-animation="maskUp" data-appear-animation-delay="250">
-									대한민국재향군인회는 보훈처 산하단체로 그 자회사인 향우실업(주)의 사업목적은 <span class="alternative-font">보훈성금 조성</span>에 있습니다.
-								</p>
-								<p>향우실업(주)가 벌어들인 수익은 국가보훈처 보훈성금으로 기탁됩니다. 기탁된 성금은 국가유공자, 보훈가족 지원, 장기복무군인 사회정착 지원 등에 사용됩니다. 또한, 지역주민 및 탈북민 등을 고용함으로써 지역사회 발전과 탈북민 자립에도 기여하고 있습니다.</p>
-							</div>
-						</div>
-						<div class="col-lg-2 appear-animation" data-appear-animation="fadeIn" data-appear-animation-delay="450">
-							<span class="thumb-info thumb-info-side-image thumb-info-side-image-right thumb-info-no-zoom">
-								<span class="thumb-info-side-image-wrapper">
-									<img src="img/about/flag.jpg" class="img-fluid" alt="국가유공자" style="width: 200px;">
-								</span>
-							</span>
-						</div>
-					</div>
-					<!-- 3rd paragraph end -->
-
+<div role="main" class="main">
+	<section class="page-header page-header-classic page-header-sm">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8 order-2 order-md-1 align-self-center p-static">
+					<h1 data-title-border>인사말</h1>
 				</div>
-
+				<div class="col-md-4 order-1 order-md-2 align-self-center">
+					<ul class="breadcrumb d-block text-md-right">
+						<li><a href="index.php">Home</a></li>
+						<li class="active">회사소개</li>
+					</ul>
+				</div>
 			</div>
+		</div>
+	</section>
 
-<?php include "include/footer.php";?>
+	<div class="container pb-1">
+
+		<div class="row pt-4">
+			<div class="col-lg-12 mb-4">
+				<h2 class="word-rotator slide font-weight-bold text-8 mb-0 appear-animation" data-appear-animation="maskUp">
+					<span>문서파쇄 전문기업, 향우실업(주)</span>
+				</h2>
+				<div class="overflow-hidden">
+					<p>향우실업(주)는 대한민국 대표 안보단체인 <span class="alternative-font"><a href="http://www.korva.or.kr" target="_blank">대한민국재향군인회</a></span>가 100% 출자하여 1978년 설립한 기업입니다.<br>
+						1970년대 정부로부터 폐지처리 대행기관으로 지정받은 이후, 오랜 기간 공공기관의 폐문서 처리를 담당하며 축적된 경험과 기술력을 바탕으로 성장해왔습니다.<br>
+						40여 년 이상의 전문성과 신뢰를 기반으로 현재는 국내 문서파쇄 분야를 선도하는 정보보안 서비스 기업으로 자리매김하고 있습니다.
+					</p>
+				</div>
+			</div>
 		</div>
 
-<?php include "include/script.php";?>
+		<!-- 2nd paragraph -->
+		<div class="row pt-4">
+			<div class="col">
+				<div class="overflow-hidden mb-3">
+					<h2 class="word-rotator slide font-weight-bold text-8 mb-0 appear-animation" data-appear-animation="maskUp">
+						<span>안전한 정보보호를 위한 보안문서 파쇄 서비스</span>
+					</h2>
+				</div>
+			</div>
+		</div>
+		<div class="row mb-4">
+			<div class="col-lg-12">
+				<div class="overflow-hidden">
+					<p>디지털 시대가 발전하면서 개인정보와 기업 정보의 보호는 더욱 중요한 과제가 되었습니다.<br>
+						오늘날 문서폐기는 단순히 종이를 없애는 작업이 아니라, 고객의 소중한 정보가 유출되지 않도록 안전하게 관리하고 완전히 제거하는 정보보안 업무입니다.<br>
+						향우실업(주)는 이러한 변화에 발맞춰 단순 폐지처리를 넘어 보안문서 파쇄, 개인정보 보호, 정보자산 폐기 분야의 전문 서비스를 제공하며 고객의 신뢰를 지켜나가고 있습니다.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="row justify-content-center mb-4">
+			<div class="col-lg-6 text-center appear-animation" data-appear-animation="fadeIn" data-appear-animation-delay="450">
+				<span class="thumb-info thumb-info-side-image thumb-info-side-image-right thumb-info-no-zoom">
+					<span class="thumb-info-side-image-wrapper">
+						<img src="img/about/shredding.jpg" class="img-fluid mx-auto d-block rounded" alt="문서파쇄">
+					</span>
+				</span>
+			</div>
+		</div>
+		<!-- 2nd paragraph end -->
 
-	</body>
+		<!-- 3rd paragraph -->
+		<div class="row pt-4">
+			<div class="col">
+				<div class="overflow-hidden mb-3">
+					<h2 class="word-rotator slide font-weight-bold text-8 mb-0 appear-animation" data-appear-animation="maskUp">
+						<span>국가유공자와 함께하는 사회적 가치 기업</span>
+					</h2>
+				</div>
+			</div>
+		</div>
+		<div class="row mb-4">
+			<div class="col-lg-12">
+				<div class="overflow-hidden">
+					<p>향우실업(주)는 대한민국재향군인회의 자회사로서, 기업 활동을 통한 수익을 <span class="alternative-font">보훈성금 조성</span>에 활용하고 있습니다.<br>
+						조성된 성금은 국가유공자와 보훈가족 지원, 장기복무 군인의 사회 정착 지원 등 국가를 위해 헌신한 분들을 위한 다양한 사업에 사용됩니다.<br>
+						또한 지역주민과 탈북민 등을 고용하여 지역사회 발전과 안정적인 자립 기반 마련에도 기여하고 있습니다.<br>
+						향우실업(주)는 최고의 문서보안 서비스를 제공하는 동시에, 사회적 책임을 실천하는 기업으로 지속 성장해 나가겠습니다.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="row justify-content-center mb-4">
+			<div class="col-lg-6 text-center appear-animation" data-appear-animation="fadeIn" data-appear-animation-delay="450">
+				<span class="thumb-info thumb-info-side-image thumb-info-side-image-right thumb-info-no-zoom">
+					<span class="thumb-info-side-image-wrapper">
+						<img src="img/about/flags.jpg" class="img-fluid mx-auto d-block rounded" alt="국가유공자">
+					</span>
+				</span>
+			</div>
+		</div>
+		<!-- 3rd paragraph end -->
+
+	</div>
+
+</div>
+
+<?php include "include/footer.php"; ?>
+</div>
+
+<?php include "include/script.php"; ?>
+
+</body>
+
 </html>
