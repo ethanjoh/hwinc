914c15d3ab71336c7b9cb959e50a5a9b3f92f1e4
ethanjoh <ethan.joh@gmail.com>
2026-06-29 14:47:06 +0900

    feat: implement admin dashboard page with billing and map navigation tools

--- admin/main.php
+++ admin/main.php
@@ -89,7 +89,7 @@ if ($numOfPay > 0) {
                 </div>
 
                 <div class="col-lg-3 col-md-6">
-                    <div class="panel panel-primary">
+                    <div class="panel panel-yellow">
                         <div class="panel-heading">
                             <div class="row">
                                 <div class="col-xs-3">
